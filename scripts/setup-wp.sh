eaf608254e96d37dbf6a5e87b8356b456c56e243
Winston Castillo <winston.castillo@wecreate.com.hk>
2021-08-06 10:48:13 +0800

    additional checking if file exists for wordpress common content folders eg. themes, upload, plugins

--- scripts/setup-wp.sh
+++ scripts/setup-wp.sh
@@ -34,12 +34,32 @@ backup_code() {
 	echo "change directory to wp-content.."
 	cd src/wp-content/
 	echo "current working directory ${PWD}"
+	echo "checking if themes already exists"
+	backup_dir="../../backup"
+	theme_file="$backup_dir/${TODAY}/themes.tar.gz"
+	echo "backup_dir:$backup_dir, $theme_file"
+	if [ -f "$theme_file" ]
+	then mv "$theme_file" "$theme_file-$(date +%H%M%S)"
+	fi
 	echo "archiving themes ..."
-	tar -czf ../../backup/${TODAY}/themes.tar.gz themes/
+	tar_result=$(tar -czf ../../backup/${TODAY}/themes.tar.gz themes/)
+	echo ${tar_result}
+	echo "checking if uploads already exists"
+	upload_file="$backup_dir/${TODAY}/uploads.tar.gz"
+	if [ -f "$upload_file" ]
+	then mv "$upload_file" "$upload_file-$(date +%H%M%S)"
+	fi
 	echo "archiving uploads ..."
-	tar -czf ../../backup/${TODAY}/uploads.tar.gz uploads/
+	tar_result=$(tar -czf ../../backup/${TODAY}/uploads.tar.gz uploads/)
+	echo ${tar_result}
+	echo "checking if plugins already exists"
+	plugins_file="$backup_dir/${TODAY}/plugins.tar.gz"
+	if [ -f "$plugins_file" ]
+	then mv "$plugins_file" "$plugins_file-$(date +%H%M%S)"
+	fi
 	echo "archiving plugins ..."
-	tar -czf ../../backup/${TODAY}/plugins.tar.gz plugins/
+	tar_result=$(tar -czf ../../backup/${TODAY}/plugins.tar.gz plugins/)
+	echo ${tar_result}
 }
 
 backup_code
